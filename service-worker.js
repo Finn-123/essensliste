4f5c5cf287c1744480021a49a06a74fcc2d463d4
Finn-123 <96596147+Finn-123@users.noreply.github.com>
2026-01-28 19:17:50 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,37 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("essensliste").then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./icon.png"
-      ])
+const CACHE_NAME = "essensliste-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./icon.png",
+  "./manifest.json",
+  "./offline.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(res => res || caches.match("./offline.html"))
     )
   );
 });
