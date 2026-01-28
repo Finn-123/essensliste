71d0ab79f8dd65649970b02c6f29ff4e57258a29
Finn-123 <96596147+Finn-123@users.noreply.github.com>
2026-01-28 19:28:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -161,12 +161,4 @@ printBtn.onclick = () => {
   doc.save("Essensliste.pdf");
 };
 
-window.addEventListener("load", () => {
-  setTimeout(() => {
-    const splash = document.getElementById("splash");
-    if (splash) splash.remove();
-  }, 2200);
-});
-
-
 render();
